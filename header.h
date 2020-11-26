4ec1c149003bd326e3be1538d10af3876cf3579a
jarfajar2314 <50899813+jarfajar2314@users.noreply.github.com>
2020-11-26 10:53:59 +0700

    dah bisa gerak + nembak

--- header.h
+++ header.h
@@ -19,15 +19,31 @@
 
 #define MAXWIDTH 47
 #define MAXHEIGHT 23
+#define FPS 15
+#define MAXBULLETS 20
+#define SPAWN 0
+#define MOVE 1
+#define BULLET_SPEED 1
 // < 68, > 67, a 97, d 100, A 56, D 68
 // Pressed Key Macro
 #define ARROW_LEFT 68
 #define ARROW_RIGHT 67
 #define K_LEFT 97
 #define K_RIGHT 100
+#define K_SPACE 32
 
 char input;
-int pos;
+int playerPos;
+// Bullet struct
+typedef struct{
+    int posX;
+    int posY;
+    int show;
+}bullet;
+// Array of Bullets
+bullet bullets[MAXBULLETS];
+// Bullet iterator
+int currentBullet;
 
 // Create getch func
 static struct termios old, current;
@@ -36,8 +52,13 @@ void resetTermios(void);
 char getch_(int echo);
 char getch(void);
 char getche(void);
+// ===================
 
 void *getInput(void *vargp); // Input from User
 void *drawArea(void *vargp); // Draw area
-int msleep(long msec);
-void move(); // Move User Character
+int msleep(long msec); // Milisecond Sleep
+void control(); // control User Character
+void fps(int frame); // frame per second
+void shoot(int mode);
+void update();
+int checkBullet(int x, int y);
